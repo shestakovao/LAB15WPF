cb26bf7a458694f73999f7747eb2fa7bb2cbdf92
Алексей Шестаков <shestakov_ao@bk.ru>
2022-12-10 19:44:16 +0700

    Добавьте файлы проекта.

---
+++ LAB15WPF/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Markup;
+
+namespace LAB15WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            docViewer.ClearValue(FlowDocumentScrollViewer.DocumentProperty);
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open("1.xaml", FileMode.Open))
+            {
+                docViewer.Document = XamlReader.Load(fs) as FlowDocument;
+            }
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open("1.xaml", FileMode.Create))
+            {
+                XamlWriter.Save(docViewer.Document, fs);
+            }
+        }
+    }
+}
